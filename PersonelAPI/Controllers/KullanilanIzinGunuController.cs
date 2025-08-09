c6ff085b144ad4a135883792dd1bc22f46937773
mustafalmazz <mustafagfb19077091@gmail.com>
2025-08-09 18:12:10 +0300

    Yillik ve Kullanılanizin controller

---
+++ PersonelAPI/Controllers/KullanilanIzinGunuController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonelAPI.Models;
+
+namespace PersonelAPI.Controllers
+{
+    public class KullanilanIzinGunuController : ControllerBase
+    {
+        private readonly PersonelDbContext _context;
+        public KullanilanIzinGunuController(PersonelDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<KullanilanIzinGunu>>> GetKullanilanIzinGunleri()
+        {
+            return await _context.KullanilanIzinGunleri.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<KullanilanIzinGunu>> GetKullanilanIzinGunu(int id)
+        {
+            var izin = await _context.KullanilanIzinGunleri.FindAsync(id);
+            if (izin == null)
+            {
+                return NotFound();
+            }
+            return izin;
+        }
+    }
+}
